340fc4c721670f6aca38020a9d61f1e3656e858f
ChickenDodger <LTLawson1@outlook.com>
2026-03-07 22:21:18 +0000

    Initial commit

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {navLinks} from "../Constants/index.js";
+
+export const NavBar = () => {
+    return (
+        <header>
+            <nav>
+                <img  src="public/logo.svg" alt="Apple logo" />
+
+                <ul>
+                    {navLinks.map(({ label }) => (
+                        <li key={label}>
+                            <a href={label}>{label}</a>
+                        </li>
+                        ))}
+                </ul>
+
+                <div className="flex-center gap-3">
+                    <button>
+                        <img  src="public/search.svg" alt="Search" />
+                    </button>
+                    <button>
+                        <img  src="public/cart.svg" alt="Cart" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
+}
